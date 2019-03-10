d59fbe50426a1ebde775e139f064f04bfd6ce5fd
Christoffer Braun <braunc@udel.edu>
2019-03-09 23:25:12 -0500

    setup model class, added attributes to model

--- src/Model.java
+++ src/Model.java
@@ -8,3 +8,39 @@
  * provide direction
  * provide location
  **/
+
+public class Model {
+	private int xloc = 0;
+	private int yloc = 0;
+	private int xIncr = 8;
+	private int yIncr = 2;
+    private int frameWidth;
+    private int frameHeight;
+    private int imgWidth;
+    private int imgHeight;
+    
+    public Model (int fWidth,int fHeight,int iWidth,int iHeight) {
+    		frameWidth = fWidth;
+    		frameHeight = fHeight;
+    		imgWidth = iWidth;
+    		imgHeight = iHeight;
+    }
+    
+    public void updateLocationAndDirection() {
+    	// COLLISION DETECTION
+    	if (xloc < 0 || xloc >= frameWidth - imgWidth) {
+    		xIncr*=-1;
+    	} // out of bounds horizontally
+    	
+    	if (yloc < 0 || yloc >= frameHeight - imgHeight) {
+    		yIncr*=-1;
+    	} // out of bounds vertically
+    	
+    	// UPDATE POSITION
+    		xloc += xIncr;
+    		yloc += yIncr;
+    		
+    	// DETERMINE DIRECTION
+    		
+    }
+}
